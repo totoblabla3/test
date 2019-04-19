37f6e2589ddbbc3b4b529cca72cd58725a608cd6
totoblabla3 <totoblabla3@users.noreply.github.com>
2019-04-20 01:22:49 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,3 +1,11 @@
 VK.init(function() {
+     post()
   }, function() {
      alert("NO");
+}, '5.95');
+
+
+function post(){
+VK.api("wall.post", {"message": "Hello!", "v":"5.73"}, function (data) {
+    alert("Post ID:" + data.response.post_id); 
+});}
