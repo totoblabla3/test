3ea9f63797a4d6012bb3ea42e5eb74254bed6024
totoblabla3 <totoblabla3@users.noreply.github.com>
2019-04-21 19:29:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ server.on('request', (req, res) => {
 	console.log(req.method);
 	console.log(req.headers);
  if (req.url === '/index' || req.url === '/') {
- 	res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})	
+ 	
+
+ res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})	
  var readhtml =fs.createReadStream(__dirname + '/index.html', 'utf8')
  readhtml.pipe(res) 
 } else {
-	res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'})	
+	
+ res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'})	
  var readhtml =fs.createReadStream(__dirname + '/404.html', 'utf8')
  readhtml.pipe(res) 
- 
 }
  
 
 });
-server.listen(process.env.PORT); 
+ server.listen(process.env.PORT); 
+//server.listen(3000, function () {console.log("Сервер работает")});
