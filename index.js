18782116a3f44903f2ced8658266a321313c7a45
totoblabla3 <totoblabla3@users.noreply.github.com>
2019-04-21 18:43:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ server.on('request', (req, res) => {
 	console.log(req.method);
 	console.log(req.headers);
  if (req.url === '/index' || req.url === '/') {
- 	res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})	
- var readhtml =fs.createReadStream(__dirname + '/index.html', 'utf8')
+ 	res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'})	
+ var readhtml =fs.createReadStream(__dirname + '/404.html', 'utf8')
  readhtml.pipe(res) 
 } else {
-	res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'})	
- var readhtml =fs.createReadStream(__dirname + '/404.html', 'utf8')
+	
+ res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})	
+ var readhtml =fs.createReadStream(__dirname + '/index.html', 'utf8')
  readhtml.pipe(res) 
 }
  
