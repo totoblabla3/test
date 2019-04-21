9f2ea03681f0f7b722d0e8f1ce8cf45f8c3d7b95
totoblabla3 <totoblabla3@users.noreply.github.com>
2019-04-21 13:52:29 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -24,7 +24,7 @@ function title1(){
 
  function title1update(){
 
- VK.api("appWidgets.update", {"title": "Привет","text": "qq", "v":"5.73"}, 'text' ,function (data) {
+ VK.api("appWidgets.update", {"title": "Привет","text": "qq", "type": "text", "v":"5.73"} ,function (data) {
 	document.getElementById("text1").textContent=data.response
 })
 }  ////new Date()
