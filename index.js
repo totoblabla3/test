c68ebecb5905d36640190e8e2f692a66d6bd223d
totoblabla3 <totoblabla3@users.noreply.github.com>
2019-04-21 23:41:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use('/public', express.static('public'));
+
 app.get('/', function(req, res){
     res.sendFile (__dirname + "/index.html");
 });
