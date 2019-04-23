af6a55f18edad4c95ed512bf6c47e45f6881f109
totoblabla3 <totoblabla3@users.noreply.github.com>
2019-04-23 22:45:25 +0300

    cool

--- index.js
+++ index.js
@@ -9,20 +9,7 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
 app.get('/', function (req, res) {
-    res.render('start', {
-        first: "0"
-    });
-});
-
-app.post('/', urlencodedParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    if (req.body.status == 0){
-        res.render('start', {first: "off"});
-    } else {
-        res.render('start', {first: "on"});
-    }
-    
-
+    res.render('start');
 });
 
 app.listen(process.env.PORT); 
