47f148ad1c1ac8c013e018d3c6248dab77dc9a8c
totoblabla3 <totoblabla3@users.noreply.github.com>
2019-04-25 03:06:59 +0300

    add bd easyvk

--- index.js
+++ index.js
@@ -3,44 +3,43 @@ var app = express();
 var bodyParser = require('body-parser');
 const easyvk = require('easyvk');
 
+
 var urlencodedParser = bodyParser.urlencoded({
     extended: false
 });
 app.set('view engine', 'ejs');
-
 app.use('/public', express.static('public'));
-
 app.get('/', function (req, res) {
     res.render('start');
 });
-
-app.listen(process.env.PORT); 
-//app.listen(3000);
+//app.listen(process.env.PORT); 
+app.listen(process.env.PORT || 3000);
 //http://localhost:3000/
 //node index
 
-// easyvk({
-//     access_token: 'd4526fbdd4526fbdd4526fbda7d43877b4dd452d4526fbd8892599a407521fd05d99a7a',//'7bdfcd69418e3190f90ecacc59f0c84b6b97de8814a14c411b1ceadb9b9d1f893d5beff8ff9bb217fd735,
-//   }).then(vk => {
+
+
+
+easyvk({
+    access_token: '7bdfcd69418e3190f90ecacc59f0c84b6b97de8814a14c411b1ceadb9b9d1f893d5beff8ff9bb217fd735', //'d4526fbdd4526fbdd4526fbda7d43877b4dd452d4526fbd8892599a407521fd05d99a7a',
+  }).then(vk => {
   
-//    // console.log(vk.session.app_id);
+   // console.log(vk.session.app_id);
+
+  //  vk.call('execute', {
+  //   code: 'API.'
+  // }); 
+    startloop();
+
+    function startloop() { 
+      var date = new Date();
+      vk.call('appWidgets.update', {
+        type: 'text',
+        code: 'return { "title": "Hey", "text": "'+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+'"};'
+      });
+      setTimeout(startloop, 11000);
+    }
     
-//     vk.call('appWidgets.update', {
-//       title: "Привет",
-//       text: new Date(),
-//       type: "text",
-//       });
   
-//   })
+  });
 
-// var iloop = 1;
-//     setTimeout(startloop, 3000);
-
-// //     function startloop() {
-// //         console.log("yes: " + iloop);
-
-// //         iloop++;
-// //         if (iloop < 100) {
-// //             setTimeout(startloop, 3000);
-// //         }
-// //     }
--- public/js/script1.js
+++ public/js/script1.js
@@ -1,9 +1,7 @@
-var i = 0;
-var toi = 10;
+
 window.onload = function () {
 
    VK.init(function () {
-     // alert("NO");
    }, function () {
       alert("NO logi");
    }, '5.95');
@@ -11,37 +9,5 @@ window.onload = function () {
 }
 
 function addkey() {
-
       VK.callMethod("showGroupSettingsBox", 4096);
-   
 }
-
-function title1() {
-   VK.callMethod('showAppWidgetPreviewBox', 'text', 'return {\
-    "title": "Привет",\
-    "text": "' + new Date() + '"\
-};');
-}
-
-function title1update() {
-
-   VK.api("appWidgets.update", {
-      "title": "Привет",
-      "text": "qq",
-      "type": "text",
-      "v": "5.73",
-      "access_token": token
-   }, function (data) {
-      document.getElementById("text1").textContent = data.error.error_msg
-   })
-} ////new Date()
-
-//if (toi>i) {
-//setTimeout(title1update(), 11000);
-//		i++	
-//}
-
-
-//VK.api("wall.post", {"message": "Hello!", "v":"5.73"}, function (data) {
-//    alert("Post ID:" + data.response.post_id); 
-//});
