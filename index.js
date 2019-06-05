44b1bf67a65926cfa141b7954d173e28fc197607
totoblabla3 <totoblabla3@users.noreply.github.com>
2019-06-06 00:47:57 +0300

    non

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const vk = require("./js/vk.js");
 const pl = require("./js/test.js"); 
 const sleep = require('util').promisify(setTimeout);
 var oldbody;
-//pl.start();
+pl.start();
 
 var urlencodedParser = bodyParser.urlencoded({
   extended: false
