bf3b6ae4cc79f48c2cea5cc115c2a3b4b33ffd8d
Сергей <totoblabla@MacBook-Pro-New.local>
2019-07-02 13:21:05 +0300

    non

--- js/test.js
+++ js/test.js
@@ -88,10 +88,13 @@ var WidgetUptateLoop = async function(){
 };
 
 var start = async function (){
-    console.log("------->1");
+    
 g.giftSendAuch();
+console.log("------->1");
 await bd.Auch();
+console.log("------->2");
 await vk.Auch(); 
+console.log("------->3");
 console.log("auch ok");
 Usersstat = await bd.UsersGet();
 await vk.PostsGet();              
